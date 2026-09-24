5f08575765e7a06fbe266bea9c29c7ac8f76530c
Kushang Acharya <kushangacharya8830@gmail.com>
2026-09-24 13:41:17 +0530

    feat: add root layout, feedback widget component, and global styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Chatbot from "@/components/Chatbot";
 import BackToTop from "@/components/BackToTop";
+import FeedbackWidget from "@/components/FeedbackWidget";
 import Preloader from "@/components/Preloader";
 import Script from "next/script";
 
@@ -363,6 +364,7 @@ export default function RootLayout({ children }) {
         <Footer />
         <Chatbot />
         <BackToTop />
+        <FeedbackWidget />
       </body>
     </html>
   );
